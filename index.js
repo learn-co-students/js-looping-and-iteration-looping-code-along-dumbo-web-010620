c7b630dced670de0aa322ecc1ac760465865f586
JoelBly <jtb6179@g.rit.edu>
2020-02-17 19:15:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(name, type){
+    
+   // let thing = name.split(",")
+    // for (let i = 0; i < thing.length; i++) {
+    // console.log(`Thank you, ${thing[i]}, for the wonderful ${type} gift!`);
+// }
+    let thankYouCards = []    
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${type} gift!`);
+    }
+    return thankYouCards
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num)
+        num -= 1
+    }
+    console.log(num);   
+}

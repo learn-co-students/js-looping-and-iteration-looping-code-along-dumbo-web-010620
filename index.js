41336ca40b153678721bb9293217a39b56c6686a
Isaac Avilez <isaac1180@aol.com>
2020-02-18 17:12:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name) {
+    let newArr = []
+    for (let i = 0; i < name.length; i++) {
+      newArr.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return newArr;
+  }
+  
+  function countDown(num) {
+    while (num > 0) {
+      console.log(num);
+      num -=1;
+    }
+    console.log(num);
+  }
